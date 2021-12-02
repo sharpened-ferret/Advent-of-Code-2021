88bc2302005901c6b6100496ad9ee9616c98c5aa
Neil Woodhouse <clonesupreme@gmail.com>
2021-12-02 19:54:05 +0000

    Added day 2, task 2

--- day2.py
+++ day2.py
@@ -18,4 +18,27 @@ def task1(input_filepath):
                 print("Error: Unexpected Instruction: '{0}'".format(keywords[0]))
     return(horizontal_pos * depth)
 
-print(task1("data/day2/input_1.txt"))
+def task2(input_filepath):
+    with open(input_filepath, "r") as file:
+        instructions = file.readlines()
+    
+    horizontal_pos = 0
+    depth = 0
+    aim = 0
+
+    for instruction in instructions:
+        keywords = instruction.split(" ")
+        match keywords[0]:
+            case "forward":
+                horizontal_pos += int(keywords[1])
+                depth += aim * int(keywords[1])
+            case "down":
+                aim += int(keywords[1])
+            case "up":
+                aim -= int(keywords[1])
+            case _: 
+                print("Error: Unexpected Instruction: '{0}'".format(keywords[0]))
+    return(horizontal_pos * depth)
+
+print("Task 1: {0}".format(task1("data/day2/input_1.txt")))
+print("Task 2: {0}".format(task2("data/day2/input_1.txt")))
